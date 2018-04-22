8dc786a457fadc92cac969f17e12b80ada967ee0
Tobe O <thosakwe@gmail.com>
2018-04-22 19:06:43 -0400

    Update Travis script

--- travis.sh
+++ travis.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 CORES=2
+DIRNAME=`dirname $0`
 set -e
 
 function run_tests() {
-  cd $1 && pub get && pub run test -j $CORES
+  cd "$DIRNAME/$1" && pub get && pub run test -j $CORES
 }
 
 run_tests ast
