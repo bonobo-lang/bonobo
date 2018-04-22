03244fa37020f71a7b4636a2f8922e7da96f6deb
Tobe O <thosakwe@gmail.com>
2018-04-22 19:11:35 -0400

    Fix Travis script

--- travis.sh
+++ travis.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 CORES=2
-DIRNAME=`dirname $0`
+DIRNAME="$(cd "$(dirname "$0")" ; pwd -P )"
 set -e
 
 function run_tests() {
   cd "$DIRNAME/$1" && pub get && pub run test -j $CORES
+  cd $DIRNAME
 }
 
 run_tests ast
