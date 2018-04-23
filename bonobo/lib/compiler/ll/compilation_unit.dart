001d02e38e5ebb3ebd02d9ca4e6cdfce23f8ccc6
Tobe O <thosakwe@gmail.com>
2018-04-23 01:28:12 -0400

    Create base LL library

---
+++ bonobo/lib/compiler/ll/compilation_unit.dart
@@ -0,0 +1,6 @@
+part of bonobo.compiler.ll;
+
+@Serializable(autoIdAndDateFields: false)
+class _CompilationUnit {
+
+}
--- bonobo/lib/compiler/low_level_compiler.dart
+++ bonobo/lib/compiler/low_level_compiler.dart
@@ -1,6 +1,11 @@
 import '../ir/ir.dart' as ir;
+import 'll/ll.dart' as ll;
 import 'base_compiler.dart';
 
-class LowLevelCompiler extends BonoboCompiler {
+class LowLevelCompiler extends BonoboCompiler<ll.CompilationUnit> {
 
+  @override
+  ll.CompilationUnit compile(ir.Module module) {
+    return null;
+  }
 }
