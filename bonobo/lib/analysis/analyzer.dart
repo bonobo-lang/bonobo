f34c07036e629fc2c3c9ab5ecea27a8116c55daf
Tobe O <thosakwe@gmail.com>
2018-04-22 23:28:27 -0400

    Broke out StatementAnalyzer

--- bonobo/lib/analysis/analyzer.dart
+++ bonobo/lib/analysis/analyzer.dart
@@ -4,6 +4,7 @@ class BonoboAnalyzer {
   final List<BonoboError> errors = [];
   final BonoboModuleSystem moduleSystem;
   ExpressionAnalyzer expressionAnalyzer;
+  StatementAnalyzer statementAnalyzer;
   TypeAnalyzer typeAnalyzer;
 
   //final Parser parser;
@@ -13,6 +14,7 @@ class BonoboAnalyzer {
 
   BonoboAnalyzer(this.moduleSystem) {
     expressionAnalyzer = new ExpressionAnalyzer(this);
+    statementAnalyzer = new StatementAnalyzer(this);
     typeAnalyzer = new TypeAnalyzer(this);
   }
 
@@ -146,105 +148,11 @@ class BonoboAnalyzer {
   }
 
   Future analyzeFunction(BonoboFunction function) async {
-    function.body = await analyzeControlFlow(function);
+    function.body = await statementAnalyzer.analyzeControlFlow(function);
 
     if (function.declaration.signature.returnType == null) {
       // Attempt to infer the return type, if none is specified
       function.returnType = function.body.returnType ?? BonoboType.Root;
     }
-  }
-
-  Future<ControlFlow> analyzeControlFlow(BonoboFunction function) async {
-    return await analyzeBlock(
-        function.declaration.body.body, function, function.scope, false);
-  }
-
-  // TODO: Infer the type of parameters from the functions they are called against
-
-  Future<ControlFlow> analyzeBlock(
-      List<StatementContext> statements,
-      BonoboFunction function,
-      SymbolTable<BonoboObject> scope,
-      bool deadCode) async {
-    var flow = new ControlFlow();
-    FileSpan deadCodeSpan;
-    bool codeWasInitiallyAlive = !deadCode;
-
-    for (var stmt in statements) {
-      if (!codeWasInitiallyAlive) {
-        continue;
-      } else if (deadCode) {
-        deadCodeSpan =
-            deadCodeSpan == null ? stmt.span : deadCodeSpan.expand(stmt.span);
-        continue;
-      }
-
-      flow.statements.add(stmt);
-
-      if (stmt is ExpressionStatementContext) {
-        if (stmt.expression is! CallExpressionContext &&
-            stmt.expression is! AssignmentExpressionContext) {
-          errors.add(new BonoboError(
-              BonoboErrorSeverity.warning,
-              'Dead code - expression is neither a call, nor an assignment.',
-              stmt.expression.span));
-        }
-
-        await expressionAnalyzer.resolve(stmt.expression, function, scope);
-      }
-
-      if (stmt is ReturnStatementContext) {
-        var value = await expressionAnalyzer.resolve(stmt.expression, function, scope);
-        deadCode = true;
-
-        if (function.returnType != null) {
-          if (value.type.isAssignableTo(function.returnType))
-            flow.returnType = value.type;
-          else {
-            errors.add(new BonoboError(
-                BonoboErrorSeverity.error,
-                "'${function.name}' is declared to return a value of type "
-                "'${function.returnType}', but it "
-                "returns a value of type '${value.type}'.",
-                stmt.expression.span));
-            flow.returnType = BonoboType.Root;
-          }
-        } else {
-          flow.returnType = value.type;
-        }
-      }
-
-      if (stmt is VariableDeclarationStatementContext) {
-        var childScope = stmt.scope = scope.createChild();
-
-        for (var decl in stmt.declarations) {
-          try {
-            childScope.create(
-              decl.name.name,
-              value: await expressionAnalyzer.resolve(
-                  decl.initializer, function, childScope),
-              constant: decl.isImmutable,
-            );
-          } on StateError catch (e) {
-            errors.add(new BonoboError(
-              BonoboErrorSeverity.error,
-              e.message,
-              decl.name.span,
-            ));
-          }
-        }
-
-        var childFlow = stmt.flow =
-            await analyzeBlock(stmt.context, function, childScope, deadCode);
-        flow.children.add(childFlow);
-      }
-    }
-
-    if (deadCodeSpan != null) {
-      errors.add(new BonoboError(
-          BonoboErrorSeverity.warning, 'Dead code.', deadCodeSpan));
-    }
-
-    return flow;
   }
 }
--- bonobo/lib/analysis/statement_analyzer.dart
+++ bonobo/lib/analysis/statement_analyzer.dart
@@ -1,2 +1,100 @@
 part of bonobo.src.analysis;
 
+class StatementAnalyzer {
+  final BonoboAnalyzer analyzer;
+
+  StatementAnalyzer(this.analyzer);
+
+  Future<ControlFlow> analyzeControlFlow(BonoboFunction function) async {
+    return await analyzeBlock(
+        function.declaration.body.body, function, function.scope, false);
+  }
+
+  // TODO: Infer the type of parameters from the functions they are called against
+  Future<ControlFlow> analyzeBlock(
+      List<StatementContext> statements,
+      BonoboFunction function,
+      SymbolTable<BonoboObject> scope,
+      bool deadCode) async {
+    var flow = new ControlFlow();
+    FileSpan deadCodeSpan;
+    bool codeWasInitiallyAlive = !deadCode;
+
+    for (var stmt in statements) {
+      if (!codeWasInitiallyAlive) {
+        continue;
+      } else if (deadCode) {
+        deadCodeSpan =
+        deadCodeSpan == null ? stmt.span : deadCodeSpan.expand(stmt.span);
+        continue;
+      }
+
+      flow.statements.add(stmt);
+
+      if (stmt is ExpressionStatementContext) {
+        if (stmt.expression is! CallExpressionContext &&
+            stmt.expression is! AssignmentExpressionContext) {
+          analyzer.errors.add(new BonoboError(
+              BonoboErrorSeverity.warning,
+              'Dead code - expression is neither a call, nor an assignment.',
+              stmt.expression.span));
+        }
+
+        await analyzer.expressionAnalyzer.resolve(stmt.expression, function, scope);
+      }
+
+      if (stmt is ReturnStatementContext) {
+        var value = await analyzer.expressionAnalyzer.resolve(stmt.expression, function, scope);
+        deadCode = true;
+
+        if (function.returnType != null) {
+          if (value.type.isAssignableTo(function.returnType))
+            flow.returnType = value.type;
+          else {
+            analyzer.errors.add(new BonoboError(
+                BonoboErrorSeverity.error,
+                "'${function.name}' is declared to return a value of type "
+                    "'${function.returnType}', but it "
+                    "returns a value of type '${value.type}'.",
+                stmt.expression.span));
+            flow.returnType = BonoboType.Root;
+          }
+        } else {
+          flow.returnType = value.type;
+        }
+      }
+
+      if (stmt is VariableDeclarationStatementContext) {
+        var childScope = stmt.scope = scope.createChild();
+
+        for (var decl in stmt.declarations) {
+          try {
+            childScope.create(
+              decl.name.name,
+              value: await analyzer.expressionAnalyzer.resolve(
+                  decl.initializer, function, childScope),
+              constant: decl.isImmutable,
+            );
+          } on StateError catch (e) {
+            analyzer.errors.add(new BonoboError(
+              BonoboErrorSeverity.error,
+              e.message,
+              decl.name.span,
+            ));
+          }
+        }
+
+        var childFlow = stmt.flow =
+        await analyzeBlock(stmt.context, function, childScope, deadCode);
+        flow.children.add(childFlow);
+      }
+    }
+
+    if (deadCodeSpan != null) {
+      analyzer.errors.add(new BonoboError(
+          BonoboErrorSeverity.warning, 'Dead code.', deadCodeSpan));
+    }
+
+    return flow;
+  }
+}
