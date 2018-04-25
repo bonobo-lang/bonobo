b5ca7036d08fd7522a4c973457b3453f0fcc5f88
Tobe O <thosakwe@gmail.com>
2018-04-24 22:04:46 -0400

    Atom language client

---
+++ atom-bonobo/lib/bonobo.js
@@ -0,0 +1,41 @@
+const cp = require('child_process');
+const {AutoLanguageClient} = require('atom-languageclient')
+
+class BonoboLanguageClient extends AutoLanguageClient {
+   getGrammarScopes() {
+     return ['source.bonobo', 'source.bnb'];
+   }
+
+   getLanguageName() {
+     return 'Bonobo';
+   }
+
+   getServerName() {
+     return 'Bonobo';
+   }
+
+   startServerProcess(projectPath) {
+    const command = 'pub';
+    const args = ['global', 'run', 'bonobo', 'language_server'];
+    const childProcess = cp.spawn(command, args);
+
+    childProcess.on("error", err =>
+      atom.notifications.addError("Unable to start the Bonobo language server.", {
+        dismissable: true,
+        buttons: [
+          {
+            text: "Install Instructions",
+            onDidClick: () => atom.workspace.open("atom://config/packages/ide-bonobo")
+          }
+        ],
+        description:
+          "Please make sure you've followed the System Requirements section in the README"
+      })
+    );
+
+    super.captureServerErrors(childProcess, projectPath);
+    return childProcess;
+  }
+}
+
+module.exports = new BonoboLanguageClient();
