a2014e08b1d84679f966955b53ac82115260259a
Tobe O <thosakwe@gmail.com>
2019-04-01 14:08:53 -0400

    Add returnType, init to unknown type

--- src/main/java/org/bonobo_lang/analysis/BonoboFunction.java
+++ src/main/java/org/bonobo_lang/analysis/BonoboFunction.java
@@ -7,11 +7,13 @@ public class BonoboFunction extends BonoboValue {
     private final BonoboModule module;
     private final BonoboScope scope;
     private final String name;
+    private BonoboType returnType;
 
     public BonoboFunction(BonoboScope scope, BonoboModule module, String name) {
         this.module = module;
         this.name = name;
         this.scope = scope;
+        returnType = new BonoboUnknownType();
     }
 
     public BonoboModule getModule() {
@@ -32,5 +34,13 @@ public class BonoboFunction extends BonoboValue {
 
     public void setDeclaration(BonoboParser.FnDeclContext declaration) {
         this.declaration = declaration;
+    }
+
+    public BonoboType getReturnType() {
+        return returnType;
+    }
+
+    public void setReturnType(BonoboType returnType) {
+        this.returnType = returnType;
     }
 }
