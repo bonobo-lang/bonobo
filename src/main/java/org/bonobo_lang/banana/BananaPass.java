53b1f2247007c06bafc6fc0a1a5988906209fd38
Tobe O <thosakwe@gmail.com>
2019-04-04 10:51:48 -0400

    Add createVariable (SSA) to BananaPass

--- src/main/java/org/bonobo_lang/banana/BananaPass.java
+++ src/main/java/org/bonobo_lang/banana/BananaPass.java
@@ -2,7 +2,11 @@ package org.bonobo_lang.banana;
 
 import org.bonobo_lang.analysis.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BananaPass {
+    private final List<BananaVariable> variables = new ArrayList<>();
     private final BonoboAnalyzer analyzer;
     private final BonoboModule module;
     private final BananaModule bananaModule;
@@ -25,6 +29,10 @@ public class BananaPass {
         return module;
     }
 
+    public List<BananaVariable> getVariables() {
+        return variables;
+    }
+
     public void run() {
         // All top-level functions are mapped to Banana functions.
         for (BonoboSymbol sym : module.getScope().getSymbols()) {
@@ -34,6 +42,29 @@ public class BananaPass {
                 if (value instanceof BonoboFunction) {
                     compileTopLevelFunction((BonoboFunction) value);
                 }
+            }
+        }
+    }
+
+    public BananaVariable createVariable(String name, BananaType type) {
+        int i = -1;
+
+        while (true) {
+            String outName = i == -1 ? name : String.format("%s%d", name, i);
+            i++;
+
+            boolean exists = false;
+            for (BananaVariable variable : variables) {
+                if (variable.getName().equals(outName)) {
+                    exists = true;
+                    break;
+                }
+            }
+
+            if (!exists) {
+                BananaVariable variable = new BananaVariable(outName, type);
+                variables.add(variable);
+                return variable;
             }
         }
     }
